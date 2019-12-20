ee8c23748dc6635dbdbd7ffb611e03f8d5778a26
ramos tito <yramost@unprg.edu.pe>
2019-12-20 01:20:45 -0500

    se agrego el lote.bat

---
+++ ramos_tito/lote.bat
@@ -0,0 +1,106 @@
+
+python app1.py  5 3
+python app1.py  16 3.14
+python app1.py  25 3.1416
+
+
+python app2.py 20 15
+python app2.py 64 82
+python app2.py 48 03
+
+
+python app3.py 54
+python app3.py 64
+python app3.py 4
+
+python app4.py 12 15 18 20
+python app4.py 13 18 05
+python app4.py 17 05 12
+
+
+python app5.py 12 19
+python app5.py 11 2
+python app5.py 48 34
+
+
+python app6.py  931547554
+python app6.py  924554123
+python app6.py  987540404
+
+python app7.py ana
+python app7.py ala
+python app7.py salas
+
+python app8.py 54454
+python app8.py 64821
+python app8.py 48036
+
+
+python app9.py 87 47 4
+python app9.py 80 50 45
+python app9.py 10 200 40
+
+python app10.py josep
+python app10.py maria
+python app10.py piero
+
+python app11.py 5000 10
+python app11.py 10000 20
+python app11.py 90000 20
+
+python app12.py 10 20
+python app12.py  50 10
+python app12.py 40 30
+
+python app13.py 50 10
+python app13.py  35 25
+python app13.py 47 30
+
+python app14.py 25 12
+python app14.py 40 10
+python app14.py 33  5
+
+python app15.py 38
+python app15.py 41
+python app15.py 45
+
+python app16.py 10 20
+python app16.py 10 50
+python app16.py 10 18
+
+python app17.py 5000 10
+python app17.py 2000 10
+python app17.py 4000 10
+
+python app18.py 428 20
+python app18.py 2406 12
+python app18.py 1474 4
+
+python app19.py  012345678901
+python app19.py  012132444542
+python app19.py  127803604417
+
+python app20.py  8 4 2
+python app20.py  25 30 4
+python app20.py  15 14 20
+
+python app21.py  8000 4 2
+python app21.py  254 30 4
+python app21.py  140 14 20
+
+python app22.py  156 8
+python app22.py  945 43
+python app22.py  439  14
+
+python app23.py  8000 4
+python app23.py  254 30
+python app23.py  140 14
+
+python app24.py  14 4
+python app24.py  23 30
+python app24.py  19 26
+
+python app25.py  13 19
+python app25.py  23 27
+python app25.py  29 15
+
