5006035c55b189025368c9b28d6a08a2c28b35e2
ramos tito <yramost@unprg.edu.pe>
2019-12-20 01:14:01 -0500

    se agrego el 22 app22

---
+++ ramos_tito/app22.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+#ejercicio22 llamar funcion: hallar_aceleracion
+masa=int(os.sys.argv[1])
+fuerza=int(os.sys.argv[2])
+j=libreria.hallar_aceleracion(masa,fuerza)
+print(j)
+
